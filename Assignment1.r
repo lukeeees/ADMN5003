619e02b2f78b681d9fc914d7af4b9894cc59aebd
L <lukelaylo@gmail.com>
2022-10-11 12:25:38 -0400

    Update R file and docs

--- Assignment1.r
+++ Assignment1.r
@@ -2,3 +2,29 @@
 # Authors: Katrina Ong, Luke Laylo
 # Data Mining for Business Analytics text
 
+#create dataframe
+df <- data.frame(age = c(25,56,65,32,41,49),
+                 income = c(49000,156000,99000,19200,39000,57000))
+df
+
+summary(df)
+
+age_mean <- mean(df$age)
+age_sd <- sd(df$age)
+
+#find z-score
+zScoreAge<- (df$age - age_mean) / age_sd
+
+#display z-score
+zScoreAge
+
+
+income_mean <-mean(df$income)
+income_sd <-sd(df$income)
+
+zScoreIncome <- (df$income - income_mean)/income_sd
+
+zScoreIncome
+
+#or 
+sapply(df,function(df) (df-mean(df))/sd(df))
