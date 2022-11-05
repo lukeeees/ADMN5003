16924a0f9476ffe234291482b9f01c4028475e9e
katrinalouiseong <katrinalouiseong@gmail.com>
2022-11-05 12:56:55 -0400

    Added Assignment 3 Files

---
+++ Assignment 3/assign3R_Luke_Katrina.R
@@ -0,0 +1,19 @@
+#==========================================
+# ADMN5003 - ASSIGNMENT 3
+#==========================================
+#------------------------------------------
+
+#==========================================
+# LOAD PACKAGES
+#==========================================
+library(readxl)
+library(forecast)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(data.table)
+library(testthat) #need to load this first to avoid problems with psych::describe
+library(psych)
+
+
+
